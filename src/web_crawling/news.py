f2904105d04a04a32e711378929b41eb5c310a83
SoonDaeK <soonho.kwon20@gmail.com>
2024-01-27 16:21:29 -0800

    ESPN 사이트에서 기사 제목 + GPT로 요약을 해주는 웹크롤링 파이썬 코딩입니다

---
+++ src/web_crawling/news.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests
+from openai import OpenAI
+
+client = OpenAI(api_key = "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", organization = "org-wtrrt7KeUZ4jD9BpsDIGnpd2")
+headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/100.0.48496.75" }
+
+espn_home = "https://www.espn.com"
+##tennis_home = "https://www.tennis.com/news"
+
+# ESPN 크롤링
+url = espn_home + "/tennis/"
+original_html = requests.get(url, headers=headers)
+html = BeautifulSoup(original_html.text, "html.parser")
+
+articles = html.find_all("article", attrs={"class":"contentItem"})
+
+article_count = 0
+for context in articles:
+    try:
+        art_url = espn_home + context.find("a", attrs={"class":"contentItem__padding"})["href"]
+        news_html = requests.get(art_url, headers=headers)
+        bs_news = BeautifulSoup(news_html.text, "html.parser")
+        title = bs_news.find("header", attrs={"class":"article-header"}).contents[0].text
+        body = bs_news.find("div", attrs={"class":"article-body"})
+        body_content = body.find_all("p")
+        body_contents = "\n".join(content.text for content in body_content)
+        article = title + "\n" + body_contents
+        prompt = f"{article}\nSummrize this article shortly."
+        response = client.completions.create(
+            model = "gpt-3.5-turbo-instruct-0914",
+            prompt = prompt,
+            max_tokens = 500
+        )
+        print(f"{title}\n{response.choices[0].text}\n==================================================\n")
+        article_count = article_count+1
+    except:
+        print("\n")
