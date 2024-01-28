32522c514b5eee110b9c4da405295f2dc38eea8f
williamEkim <dongyeon0308@gmail.com>
2024-01-27 20:00:09 -0800

    updated server js file to run python script in web_crawling directory

--- app.js
+++ app.js
@@ -1,15 +1,71 @@
 const express = require ('express');
-const favicon = require('serve-favicon')
+const favicon = require('serve-favicon');
+const { spawn } = require('child_process');
+const { result } = require('lodash');
 
 // express app
 const appInstance = express();
 
 // register view engine
 appInstance.set('view engine', 'ejs');
-    // folder configuration
-    appInstance.set('views', 'views')
+// views folder configuration
+appInstance.set('views', 'views');
+
+// Full path to the Python executable
+const pythonExecutable = '/opt/homebrew/bin/python3'; // Change this to the actual path
+
+// const childPython = spawn(pythonExecutable, [`--version`]);
+
+// childPython.stdout.on('data', (data) => {
+//     console.log(`stdout: ${data}`);
+// });
+
+// childPython.stderr.on('data', (data) => {
+//     console.error(`stderr: ${data}`);
+// });
+
+// childPython.on('error', (error) => {
+//     console.error(`Error spawning Python: ${error.message}`);
+// });
+
+// childPython.on('close', (code) => {
+//     console.log(`Python process exited with code ${code}`);
+// });
+
+
+
 // path configuration
 // appInstance.use(express.static('public'));
+
+const executePython = async (script, args) => {
+    const arguments = args.map(arg => arg.toString());
+
+    // const py = spawn("python", [script, ...arguments]);
+    const py = spawn(pythonExecutable, [script, ...arguments]);
+
+    const result = await new Promise((resolve, reject) => {
+        let output;
+
+        // Get output from python script
+        py.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+            output = JSON.parse(data);
+        });
+
+        // Handle erros
+        py.stderr.on("data", (data) => {
+            console.error(`[python] Error occured: ${data}`);
+            reject(`Error occured in ${script}`);
+        });
+
+        py.on("exit", (code) => {
+            console.log(`Child process exited with code ${code}`);
+            resolve(output);
+        });
+    });
+
+    return result;
+}
 
 // favicon configuration
 appInstance.use(favicon(__dirname + '/images/icons.ico/favicon.ico'));
@@ -17,17 +73,20 @@ appInstance.use(favicon(__dirname + '/images/icons.ico/favicon.ico'));
 // listen for request
 appInstance.listen(3300);
 
-appInstance.get('/', (request, response) => {
-    // const blogs = [
-    //     { title: 'post 1', snippet: '1' },
-    //     { title: 'post 2', snippet: '2' },
-    //     { title: 'post 3', snippet: '3' }
-    // ]
+appInstance.get('/', async (request, response) => {
+    let result;
+    try {
+        result = await executePython('src/web_crawling/news.py', []);
+    } catch (error) {
+        response.status(500).json({ error: error });
+    }
 
     response.render('index', {
         title: "Home",
-        // blogs
+        result
     });
+
+
 });
 
 appInstance.get('/about', (request, response) => {
@@ -52,4 +111,18 @@ appInstance.use((request, response) => {
     response.status(404).render('404', {
         title: "404 Not Found"
     });
+});
+
+
+// APIs
+appInstance.get('/api/topnews', async (request, response) => {
+    try {
+        const result = await executePython('python/script.py', [8, 5]);
+
+        response.json({ result: result });
+    } catch (error) {
+        response.status(500).json({ error: error });
+    }
+
+    response.json({result: result});
 });
